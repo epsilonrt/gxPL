89203403ac04e7487a08dc1eb3d4e66f1d36c626
Pascal JEAN <pascal.jean@btssn.net>
2015-08-28 15:26:59 +0200

    Travail sur les utilitaires

---
+++ util/gxpl-prj/template/c/template.c
@@ -0,0 +1,46 @@
+/*
+ * template.c
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <signal.h>
+#include <unistd.h>
+#include <xPL.h>
+
+/* constants ================================================================ */
+/* macros =================================================================== */
+/* structures =============================================================== */
+/* types ==================================================================== */
+/* private variables ======================================================== */
+/* public variables ========================================================= */
+/* internal public functions ================================================ */
+
+/* private functions ======================================================== */
+// -----------------------------------------------------------------------------
+static void
+vSigIntHandler (int sig) {
+
+  printf("\neverything was closed.\nHave a nice day !\n");
+  exit(EXIT_SUCCESS);
+}
+
+/* main ===================================================================== */
+int
+main (int argc, char **argv) {
+
+  // vSigIntHandler() intercepte le CTRL+C
+  signal(SIGINT, vSigIntHandler);
+
+  printf("Press Ctrl+C to abort ...\n");
+  for (;;) {
+    // Replace the 2 lines below with your source code
+    putchar('.'); fflush(stdout);
+    sleep (1);
+  }
+
+  return 0;
+}
+/* ========================================================================== */
